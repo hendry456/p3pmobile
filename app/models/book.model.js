4183df75872056cb83dd8df39b1d86a496a1f8ef
Lab-28 <treesyamelliyani31@gmail.com>
2020-02-18 22:46:40 +0700

    perlu diperbaiki

--- app/models/book.model.js
+++ app/models/book.model.js
@@ -19,4 +19,84 @@ Book.getAll = result => {
     })
 }
 
+// get book by id
+Book.findById = (id, result) => {
+    sql.query(`SELECT * FROM books WHERE id = ${id}`, (err, res) => {
+        if(err){
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if(res.length){
+            console.log(res[0]);
+            result(null, res[0]);
+            return;
+        }
+        result({kind: "not_found"}, null);
+    });
+};
+
+Book.create = (newBook, result) => {
+    console.log(newBook);
+    sql.query("INSERT INTO books (title, description, images) VALUES (?,?,?)", [newBook.title, newBook.description, newBook.images], (err,res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        console.log(res);
+        console.log("buat buku: ", { id: res.insertId, ...newBook});
+        result(null, { id: res.insertId, ...newBook });
+    });
+};
+
+Book.updateById = ( id, Book, result) => {
+    sql.query("UPDATE Books SET title = ?, description = ?, images = ? WHERE id = ?", [Book.title, Book.description, Book.images, id], (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        if (res.affectedRows == 0) {
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        console.log("update buku: ", { id: id, ...Book });
+        result(null, { id: id, ...Book });
+    }
+    );
+};
+
+Book.remove = (id, result) => {
+    sql.query("DELETE FROM Books WHERE id = ?", id, (err, res) =>
+   {
+    if (err) {
+    console.log("error: ", err);
+    result(null, err);
+    return;
+    }
+    if (res.affectedRows == 0) {
+    // not found Book with the id
+    result({ kind: "not_found" }, null);
+    return;
+    }
+    console.log("hapus buku dengan id: ", id);
+    result(null, res);
+    });
+};
+
+Book.removeAll = result => {
+    sql.query("DELETE FROM Books", (err, res) => {
+    if (err) {
+    console.log("error: ", err);
+    result(null, err);
+    return;
+    }
+    console.log(`Menghapus ${res.affectedRows} buku`);
+    result(null, res);
+    });
+   };
+    
+
 module.exports = Book;
